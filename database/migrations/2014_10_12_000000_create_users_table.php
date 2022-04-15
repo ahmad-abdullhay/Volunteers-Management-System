9726f9bb4eaca03ef270c3b3cf9b827bf41390b3
hassan kareem <hassan.k.aldeen@sheenvalue.com>
2022-04-15 23:40:49 +0300

    Let seeder hash password by password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->date('date_of_birth');
             $table->tinyInteger('gender')->comment("(0) => Male, (1) => Female");
             $table->string('job');
-            $table->string('volunteering_history');
+            $table->text('volunteering_history');
             $table->string('location');
             $table->tinyInteger('is_active')->comment('(1) => Active, (0) => Inactive')->default(0);
 
