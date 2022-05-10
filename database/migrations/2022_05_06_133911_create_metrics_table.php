0087e1cb1e8d52ae89f3899569d97c5879549110
hassan kareem <hassan.k.aldeen@sheenvalue.com>
2022-05-10 21:55:54 +0300

    Change default description db type in metrics

--- database/migrations/2022_05_06_133911_create_metrics_table.php
+++ database/migrations/2022_05_06_133911_create_metrics_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('metrics', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->default(' ');
+            $table->text('description')->nullable();
             $table->integer('type');
             $table->timestamps();
         });
