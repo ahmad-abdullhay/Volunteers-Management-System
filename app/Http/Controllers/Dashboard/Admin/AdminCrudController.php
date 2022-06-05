2c25fa3ca506b53bf7e8aa6e554330ef5ae75fd3
yazansh <yazansh412@gmail.com>
2022-06-06 01:05:17 +0300

    done

--- app/Http/Controllers/Dashboard/Admin/AdminCrudController.php
+++ app/Http/Controllers/Dashboard/Admin/AdminCrudController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Dashboard\Admin;
 
 use App\Http\Controllers\CrudController;
 use App\Http\Requests\AdminRequest;
+use App\Http\Requests\MainRequest;
 use App\Services\Shared\BaseService;
 use App\Services\AdminService;
+use Faker\Provider\Person;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AdminCrudController extends CrudController
 {
@@ -20,5 +24,9 @@ class AdminCrudController extends CrudController
     public function __construct(AdminService $service, AdminRequest $request)
     {
         parent::__construct($service, $request);
+    }
+
+    public function CheckToken(MainRequest $request ){
+        return response()->json(["message" => "Authenticated"]);
     }
 }
