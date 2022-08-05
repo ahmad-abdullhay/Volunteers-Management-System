d70c8c10216cda53f9b8d188ba3d593aef099346
Ahmad abdullhay <ahmad.abdullhay@outlook.com>
2022-08-05 20:52:57 +0300

    small fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,11 @@ namespace App\Models;
 class Category extends BaseModel
 {
     protected $guarded = [];
+    protected $appends = ['traits'];
+
+    public function getTraitsAttribute (){
+        return CategoryTraits::where("category_id",$this->id);
+    }
 
 
 }
--- app/Models/EventCategory.php
+++ app/Models/EventCategory.php
@@ -7,9 +7,4 @@ class EventCategory extends BaseModel
     protected $table = 'event_category';
 
     public $timestamps = false;
-    protected $appends = ['traits'];
-    public function getTraitsAttribute (){
-        return CategoryTraits::where("category_id",$this->id)->get();
-    }
-
 }
